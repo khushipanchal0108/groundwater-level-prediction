1a55e1f584cf96fcf7d03a8ee3243bcbd05d0e02
khushipanchal0108 <141764457+khushipanchal0108@users.noreply.github.com>
2023-10-26 23:38:42 +0530

    Update kp.py

--- kp.py
+++ kp.py
@@ -12,9 +12,8 @@ models = {
 
 st.title("Groundwater Level Prediction")
 
-# Sliders for input values
-precipitation = st.slider("Precipitation", min_value=0.000000000, max_value=0.03133407, value=0.0, step=0.0001)
-evapotranspiration = st.slider("Evapotranspiration", min_value=-0.005239387, max_value=-0.041337304, value=0.0, step=0.0001)
+precipitation = st.slider("Precipitation", min_value=0.0, max_value=0.03133407, value=0.0, step=0.0001)
+evapotranspiration = st.slider("Evapotranspiration", min_value=-0.041337304, max_value=-0.005239387, value=0.0, step=0.0001)
 input_data = [[precipitation, evapotranspiration]]
 
 for model_name, model in models.items():
@@ -28,21 +27,20 @@ end_date = st.date_input("End Date", pd.to_datetime("2021-08-23"))
 
 if start_date > end_date:
     st.warning('End date must fall after start date.')
-elif st.button("Predict using ARIMA"):
-    try:
-        last_train_date = pd.to_datetime("2021-04-18")
-        forecast_steps = (pd.Timestamp(end_date) - last_train_date).days
-    
-        forecast_output = models["ARIMA"].forecast(steps=forecast_steps)
-        forecast_values = forecast_output[0] if isinstance(forecast_output, (list, tuple, np.ndarray)) else [forecast_output]
+else:
+    if st.button("Predict using ARIMA"):
+        try:
+            last_train_date = pd.to_datetime("2021-04-18")
+            forecast_steps = (pd.Timestamp(end_date) - last_train_date).days + 1 
 
-        desired_forecast = forecast_values[(pd.Timestamp(start_date) - last_train_date).days:]
-        
-        forecast_df = pd.DataFrame(desired_forecast, columns=["Predicted Groundwater Level"], 
-                                   index=pd.date_range(start=start_date, end=end_date))
-        
-        st.dataframe(forecast_df)
-        
-        st.line_chart(desired_forecast, use_container_width=True)
-    except Exception as e:
-        st.error(f"Error in prediction: {e}")
+            forecast_output = models["ARIMA"].forecast(steps=forecast_steps)
+            forecast_values = forecast_output[0] if isinstance(forecast_output, (list, tuple, np.ndarray)) else [forecast_output]
+
+            desired_forecast = forecast_values[(pd.Timestamp(start_date) - last_train_date).days:]
+
+            forecast_df = pd.DataFrame(desired_forecast, columns=["Predicted Groundwater Level"], 
+                                       index=pd.date_range(start=start_date, end=end_date))
+            st.dataframe(forecast_df)
+            st.line_chart(forecast_df, use_container_width=True)
+        except Exception as e:
+            st.error(f"Error in prediction: {e}")
