f79a4e21960bb30e6e92785172cd47a4fc4aae30
khushipanchal0108 <141764457+khushipanchal0108@users.noreply.github.com>
2023-10-26 21:49:49 +0530

    Update kp.py

--- kp.py
+++ kp.py
@@ -2,7 +2,6 @@ import streamlit as st
 from joblib import load
 import pandas as pd
 
-# Load the models
 models = {
     "DecisionTree": load('DecisionTree.joblib'),
     "LinearRegression": load('LinearRegression.joblib'),
@@ -17,16 +16,19 @@ precipitation = st.slider("Precipitation", min_value=-0.000000000, max_value=0.0
 evapotranspiration = st.slider("Evapotranspiration", min_value=-0.005239387, max_value=-0.041337304, value=0.0, step=0.0001)
 input_data = [[precipitation, evapotranspiration]]
 
-# Separate predict buttons for each model except ARIMA
 for model_name, model in models.items():
     if model_name != "ARIMA" and st.button(f"Predict using {model_name}"):
         prediction = model.predict(input_data)[0]
         st.write(f"{model_name} Prediction: {prediction:.2f}")
 
-# Input and predict button for ARIMA model
-date_input = st.text_input("Enter a date for ARIMA prediction (dd/mm/yyyy)", "26/10/2023")
-if st.button("Predict using ARIMA"):
-    # Convert the date_input to the format your ARIMA model expects.
-    date_for_arima = pd.to_datetime(date_input, format="%d/%m/%Y")
-    prediction = models["ARIMA"].predict(date_for_arima)
-    st.write(f"ARIMA Prediction: {prediction:.2f}")
+start_date = st.date_input("Select a start date for ARIMA prediction", pd.to_datetime("26/10/2023"))
+end_date = st.date_input("Select an end date for ARIMA prediction", pd.to_datetime("27/10/2023"))
+
+if start_date > end_date:
+    st.warning('End date must fall after start date.')
+elif st.button("Predict using ARIMA"):
+    
+    prediction = models["ARIMA"].predict(start=start_date, end=end_date)
+    
+    st.write(f"ARIMA Predictions from {start_date} to {end_date}:")
+    st.write(prediction)
